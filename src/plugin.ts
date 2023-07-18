73b7aca15135766a8cea35f21c5a9957f59e6fd4
Robin Bühler <git+r@obin.ch>
2023-07-18 11:25:59 +0000

    chore: scaffold plugin

---
+++ src/plugin.ts
@@ -0,0 +1,10 @@
+import { createPlugin } from '@inlang/core/plugin';
+
+export const plugin = createPlugin(({env}) => ({
+  id: 'openscript-ch.inlang-plugin-react-intl',
+  config: async () => {
+    return {
+
+    }
+  }
+}))
